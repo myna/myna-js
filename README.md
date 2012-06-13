0c5ac30fc929859ff3df6c70021337a9da089727
Noel Welsh <noel@untyped.com>
2012-06-13 15:54:19 +0100

    Initial commit of Myna JS client

---
+++ README.md
@@ -0,0 +1,19 @@
+myna-js
+=======
+
+Javascript client for Myna
+
+
+# Developing
+
+This project use [Grunt](https://github.com/cowboy/grunt) as its build tool. You should see the Grunt website for full install instructions. If you have Node.js and npm installed, you should be able to install grunt with `npm install -g grunt`.
+
+You also need to install [PhantomJS](http://code.google.com/p/phantomjs/downloads/list) to run the supplied tests.
+
+## Testing
+
+`grunt jasmine`
+
+## Building
+
+`grunt concat`
---
+++ spec/myna-spec.js
@@ -0,0 +1,26 @@
+describe("Myna", function() {
+  var testUuid = "45923780-80ed-47c6-aa46-15e2ae7a0e8c";
+  var experiment;
+
+  beforeEach(function() {
+    experiment = Myna(testUuid);
+  })
+
+  it("should return a suggestion when asked to", function() {
+    var flag = false;
+    var result = false;
+
+    runs(function () {
+      experiment.suggest(
+        function(suggestion) { flag = true; result = suggestion },
+        function(error) { flag = true; result = error})
+    })
+
+    waitsFor(function() { return flag; }, "The suggestion should return", 1000)
+
+    runs(function() {
+      expect(result.choice).toBeTruthy();
+      expect(result.token).toBeTruthy();
+    })
+  })
+})
